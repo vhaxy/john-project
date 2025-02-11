c44eaa75bdd10b7c7e2449207d7460fe5b047ab5
vhaxy <liam.makoni@gmail.com>
2025-02-11 10:05:09 +0000

    reformatted file structure and fixed some issues

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,20 @@
-console.log('hello');
+function handleSubmit(event) {
+    event.preventDefault(); // Prevent page reload
+
+    var form = document.getElementById("contactForm");
+    var formData = {
+      firstName: form.firstName.value,
+      lastName: form.lastName.value,
+      email: form.email.value,
+      phone: form.phone.value
+    };
+
+    google.script.run.withSuccessHandler(function(response) {
+      if (response === "Success") {
+        alert("Thank you for your inquiry! A confirmation email has been sent.");
+        form.reset();
+      } else {
+        alert("There was an error processing your inquiry. Please try again later.");
+      }
+    }).processForm(formData);
+  }
